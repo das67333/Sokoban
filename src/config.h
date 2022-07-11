83af116041278e5050378ba63bc41fac4b39f84f
das67333 <kazimov.sp@phystech.edu>
2022-07-11 16:48:32 +0300

    created folder for resources

--- src/config.h
+++ src/config.h
@@ -7,10 +7,10 @@ enum class CharacterDirection { left, right, down, up, undefined };
 namespace config {
 const uint32_t fps_max = 15;
 const uint32_t cell_size = 64;
-const std::string sprites_path = "../sprites.png";
-const std::string xml_path = "../sprites.xml";
+const std::string sprites_path = "../resources/sprites.png";
+const std::string xml_path = "../resources/sprites.xml";
 const std::string levels_path = "../levels/";
-const std::string font_path = "../arial.ttf";
+const std::string font_path = "../resources/arial.ttf";
 const uint32_t font_size = 15;
 const float fps_x = 15., fps_y = 10.;
 const float moves_number_x = 15., moves_number_y = 30.;
